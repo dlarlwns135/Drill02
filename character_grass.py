27d07a7680189301014a30e4d7165ccc92fcc42d
dlarlwns135 <dlarlwns135@naver.com>
2023-09-18 12:27:47 +0900

    Drill02 2019180035 임기준
    완성

---
+++ character_grass.py
@@ -0,0 +1,66 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+# fill here
+from pico2d import *
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+mode_1 = True
+mode_2 = False
+
+x = 0
+y = 90
+gak = 0
+d = 0.001
+
+while (1):
+    if mode_1 == True:
+        if y == 90 and x <800:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            x = x + 2
+            delay(d)
+        elif y<600 and x==800:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            y = y + 2
+            delay(d)
+        elif y==600 and x>0:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            x = x - 2
+            delay(d)
+        elif x==0 and y>90:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            y = y - 2
+            delay(d)
+            if x==0 and y ==90:
+                mode_1 = False
+                mode_2 = True
+    elif mode_2 == True:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        gak = gak + 10
+        radian = math.radians(gak)
+        x = 400 + 250 * math.cos(radian) 
+        y = 300 - 250 * math.sin(radian) 
+        delay(0.1)
+        if gak > 360:
+            x = 0
+            y = 90
+            mode_1 = True
+            mode_2 = False
+
+close_canvas()
+
